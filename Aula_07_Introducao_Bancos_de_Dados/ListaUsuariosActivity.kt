b59c7c88451554af78c1802c0d26b19c4542598a
rafamoc <rafamoc@gmail.com>
2024-11-07 10:41:01 -0300

    Create ListaUsuariosActivity.kt

---
+++ Aula_07_Introducao_Bancos_de_Dados/ListaUsuariosActivity.kt
@@ -0,0 +1,41 @@
+package br.com.uniceub.android
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListaUsuariosActivity : AppCompatActivity() {
+
+    private lateinit var conexaoDB: ConexaoDB
+    private lateinit var usuario: Usuario
+    private lateinit var layoutContainer: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lista_usuarios)
+
+        conexaoDB = ConexaoDB(this)
+        usuario = Usuario(conexaoDB)
+        layoutContainer = findViewById(R.id.LinearLayout2)
+
+        // Chama o método para buscar os nomes dos usuários e exibi-los
+        mostrarUsuarios()
+    }
+
+    private fun mostrarUsuarios() {
+        val nomes = usuario.getAllUserNames()
+
+        for (nome in nomes) {
+            val textView = TextView(this)
+            textView.text = nome
+            textView.textSize = 18f
+            textView.setPadding(16, 16, 16, 16)
+
+            layoutContainer.addView(textView)
+        }
+    }
+}
